ae051a28fb3f9bfe7f29955243b592a14dc0538e
Thomas Schalldach <register@thomas-schalldach.de>
2015-10-17 17:28:54 +0300

    changes added page for question submit

--- JavaEE_Project/src/java/controler/DataController.java
+++ JavaEE_Project/src/java/controler/DataController.java
@@ -5,8 +5,14 @@
  */
 package controler;
 
+import java.util.LinkedList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import model.Answers;
 import model.Questions;
 
 /**
@@ -16,12 +22,35 @@ import model.Questions;
 @ManagedBean
 @RequestScoped
 public class DataController {
-    Questions question;
-
+    private String question;
+    private String[] answers;
+    private EntityManager em;
+    private EntityTransaction t;
+    
+    
     /**
      * Creates a new instance of DataControler
      */
     public DataController() {
+        em = Persistence.createEntityManagerFactory("JavaEE_ProjectPU").createEntityManager();
+        answers = new String[4];
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+    public void addQuestion(){
         
     }
     
